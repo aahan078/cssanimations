e0b73e8f79a024b6bae77192095931291d6a67fe
aahan078 <aahan.solanki@vkaps.com>
2023-04-10 17:01:42 +0530

    adding animations

---
+++ index.js
@@ -0,0 +1,12 @@
+let showanime = document.getElementById("btn--1");
+let show = document.getElementById("show")
+
+showanime.addEventListener('click',myFunc);
+
+function myFunc(){
+       
+   show.style.display="none"
+   
+
+}
+
